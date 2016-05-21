0c7ad0db9c45aa578b12f7c70ae4784a14cec08d
Rahul Ramachandra <hijaz.apple@gmail.com>
2016-05-21 13:01:12 +0530

    Phase1 feature1 Commit

--- SampleGitTest/ViewController.m
+++ SampleGitTest/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"phase1 feature1 Commit");
 }
 
 - (void)didReceiveMemoryWarning {
