6b51557215968831a74673977ca04f9598576cb1
Rahul Ramachandra <hijaz.apple@gmail.com>
2016-05-21 12:20:53 +0530

    Initial Commit

---
+++ SampleGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleGitTest
+//
+//  Created by Rahul Ramachandra on 21/05/16.
+//  Copyright © 2016 Rahul Ramachandra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleGitTest
+//
+//  Created by Rahul Ramachandra on 21/05/16.
+//  Copyright © 2016 Rahul Ramachandra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
